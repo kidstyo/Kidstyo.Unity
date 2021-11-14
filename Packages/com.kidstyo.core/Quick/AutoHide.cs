ec51d8e8aee46af7dba7470c8bd6de7c6fa4aa92
wwnje <wwnje@wwnje.com>
2021-11-14 20:01:44 +0800

    + autohide

---
+++ Packages/com.kidstyo.core/Quick/AutoHide.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class AutoHide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+}
