221fc3de954ddeeb85aa6405a1eb5fba4561c106
einli <einli@tencent.com>
2022-06-28 18:17:14 +0800

    + Utils

---
+++ Packages/com.kidstyo.utils/Scripts/LayerMaskUtil.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public static class LayerMaskUtil
+{
+    public static bool IsInLayerMask(int layer, LayerMask layerMask)
+    {
+        return layerMask == (layerMask | (1 << layer));
+    }
+}
